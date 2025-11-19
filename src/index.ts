39525e6f06dab4621769b32b4018a435dca10f54
maamokun <sachadeco214@gmail.com>
2025-11-19 18:57:19 +0900

    feat: Dockerize and Elysiaify

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,7 @@ import { Elysia } from "elysia";
 import Redis from "ioredis";
 import { check } from "./check";
 
-const redis = new Redis({
-	host: process.env.REDIS_HOST || "localhost",
-	port: Number(process.env.REDIS_PORT) || 6379,
-	password: process.env.REDIS_PASSWORD,
-	db: Number(process.env.REDIS_DB) || 0,
-});
+const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
 
 const app = new Elysia()
 	.get("/check/:ip", async ({ params: { ip } }) => {
